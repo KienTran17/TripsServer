1a00967d6ed9dec3fbdfa199032b1079a817d4a7
Kien Tran <INI\kien.tran@pc00422.infonam.com>
2017-11-06 16:44:12 +0700

    make it better

--- server/api/users/getProfile.js
+++ server/api/users/getProfile.js
@@ -1,5 +1,4 @@
 const getProfile = require('../../model/users/getProfile')
-const { verify } = require('../../../config/jwt')
 module.exports = (req, res, next) => {
     const { token } = req.session;
     getProfile(token)

645e4f9b390c773c75c10470c3415b9ffb6ec4e3
Kien Tran <INI\kien.tran@pc00422.infonam.com>
2017-11-06 16:51:15 +0700

    make it better

--- server/api/users/getProfile.js
+++ server/api/users/getProfile.js
@@ -1,6 +1,6 @@
 const getProfile = require('../../model/users/getProfile')
 module.exports = (req, res, next) => {
-    const { token } = req.session;
+    const token = req.session.token;
     getProfile(token)
         .then(obj => res.send(obj))
         .catch(err => res.send('error'))

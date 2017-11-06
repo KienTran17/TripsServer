0dc1669842a485ac80e0f0d250d69cc2909ea618
Kien Tran <INI\kien.tran@pc00422.infonam.com>
2017-11-06 18:02:15 +0700

    sth

--- server/api/users/register.js
+++ server/api/users/register.js
@@ -2,38 +2,40 @@ const { sign } = require('../../../config/jwt');
 const register = require('../../model/users/register');
 const bcrypt = require('bcrypt')
 module.exports = (req, res, next) => {
-    const { 
-        username, 
-        twitter_link, 
-        phone, 
-        password, 
-        nickname, 
-        last_name, 
-        instagram_link, 
-        google_link, 
-        flick_link, 
-        first_name, 
-        facebook_link, 
-        email, 
-        cover_link, 
-        birthday, 
-        avatar_link, 
-        address 
+    const {
+        username,
+        twitter_link,
+        phone,
+        password,
+        nickname,
+        last_name,
+        instagram_link,
+        google_link,
+        flick_link,
+        first_name,
+        facebook_link,
+        email,
+        cover_link,
+        birthday,
+        avatar_link,
+        address
     } = req.body;
-    
-    register( username, twitter_link, phone, password, nickname, last_name,
-        instagram_link, google_link, flick_link, first_name, facebook_link, 
-        email, cover_link, birthday, avatar_link, address )
-        .then(isOk => {
-            if (isOk === true)
-                sign({ username })
-                    .then(token => {
-                        req.session.token = token;
-                        res.send(token)
-                    })
-                    .catch(err => console.log(err + ''))
-        })
-        .catch(err => {
-            res.send(err)
-        })
+    bcrypt.hash(password, 10, (err, hash) => {
+        register(username, twitter_link, phone, hash, nickname, last_name,
+            instagram_link, google_link, flick_link, first_name, facebook_link,
+            email, cover_link, birthday, avatar_link, address)
+            .then(isOk => {
+                if (isOk === true)
+                    sign({ username })
+                        .then(token => {
+                            req.session.token = token;
+                            res.send(token)
+                        })
+                        .catch(err => console.log(err + ''))
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    })
+
 }

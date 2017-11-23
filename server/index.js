f8fd8c5392417b0e1c0916135a223bfc5394447a
Kien Tran <INI\kien.tran@pc00422.infonam.com>
2017-11-23 12:33:10 +0700

    access control allow headers

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
 //route user
